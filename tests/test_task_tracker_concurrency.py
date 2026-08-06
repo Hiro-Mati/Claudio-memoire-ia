b1481039a615d1eaaba00934a6e2aedfa5f740d2
chenpengfei <chenpengfei@bytedance.com>
2026-08-06 17:40:43 +0800

    修订开源协议

--- tests/test_task_tracker_concurrency.py
+++ tests/test_task_tracker_concurrency.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2025 Beijing Volcano Engine Technology Co., Ltd.
-# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2026 Beijing Volcano Engine Technology Co., Ltd.
+# SPDX-License-Identifier: AGPL-3.0
 
 import asyncio
 import threading
